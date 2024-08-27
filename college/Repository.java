83ebe1b549d1434bb98c9b86b5b8f958f21f6111
Ajaykumar.P <147475618+AJAY-P-KUMAR@users.noreply.github.com>
2024-08-27 13:05:13 +0530

    Add files via upload

---
+++ college/Repository.java
@@ -0,0 +1,7 @@
+package com.department.college;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<StudentModel,Integer> {
+    
+}
